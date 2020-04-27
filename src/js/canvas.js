f689f26eaa2842ed3176c684902fb39c719dd523
Aleksi Partanen <aleksi.partanenn17@gmail.com>
2020-04-27 14:02:39 +0300

    Init and final commit

---
+++ src/js/canvas.js
@@ -0,0 +1,111 @@
+import { randomColor, randomIntFromRange } from './utils'
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+}
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+})
+
+addEventListener('resize', () => {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+
+    init()
+})
+
+addEventListener('click', (e) => {
+    init()
+})
+
+// Confs
+let speedY   = 3
+let speedX   = 0
+let gravity = 1
+let friction = 0.98
+let ballsAmount = 100
+
+// Objects
+class Ball {
+    constructor(x, y, dx, dy, radius, color) {
+        this.x = x
+        this.y = y
+        this.dx = dx
+        this.dy = dy
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+        c.stroke()
+        c.closePath()
+    }
+
+    update() {
+        if(this.y + this.radius + this.dy > canvas.height || 
+           this.y - this.radius - this.dy <= 0) {
+            this.dy = -this.dy * friction
+        } else {
+            this.dy += gravity
+        }
+
+        if(this.x + this.radius + this.dx> canvas.width || 
+           this.x - this.radius  < 0) {
+            this.dx = -this.dx
+        }
+
+        this.x += this.dx
+        this.y += this.dy
+        this.draw()
+    }
+}
+
+// Implementation
+let ball
+let ballArray = []
+function init() {
+    ballArray = []
+
+    ball = new Ball(canvas.width / 2, canvas.height / 2, speedY, 30, 'red')
+    
+    for(let i = 0; i < ballsAmount; i++) {
+        let dx = randomIntFromRange(-2, 2)
+        let dy = randomIntFromRange(-2, 2)
+        let radius = randomIntFromRange(8, 20)
+        let x = randomIntFromRange(radius+dx, canvas.width-radius)
+        let y = randomIntFromRange(radius+dy, canvas.height-radius)
+        let color = randomColor(colors)
+
+        ballArray.push(new Ball(x, y, dx, dy, radius, color))
+    }
+}
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+
+    for(let i = 0; i < ballArray.length; i++) {
+        ballArray[i].update()
+    }
+
+    ball.update()
+}
+
+init()
+animate()
